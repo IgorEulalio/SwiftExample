3ef2ba9203e3174898df49209a12f2bddabc29aa
IgorEulalio <igoreulalio.ie@gmail.com>
2020-08-18 14:22:51 -0300

    iniciando projeto

--- eggplant-brownie/ViewController.swift
+++ eggplant-brownie/ViewController.swift
@@ -9,12 +9,17 @@
 import UIKit
 
 class ViewController: UIViewController {
-
-    override func viewDidLoad() {
-        super.viewDidLoad()
-        // Do any additional setup after loading the view.
+    
+    @IBOutlet var nomeTextField: UITextField!
+    @IBOutlet var felicidadeTextField: UITextField!
+    
+    @IBAction func test(_ sender: Any){
+        
+        let nome = nomeTextField.text
+        let felicidade = felicidadeTextField.text
+        
+        print("comi \(nome) e fiquei com felicidade \(felicidade)")
     }
-
 
 }
 
